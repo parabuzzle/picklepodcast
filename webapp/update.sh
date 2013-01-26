08e28594d64846abd6869da3db59149ee90cba4a
Mike Heijmans <parabuzzle@gmail.com>
2013-01-26 09:24:42 -0800

    testing whenever

--- webapp/update.sh
+++ webapp/update.sh
@@ -9,7 +9,11 @@ bundle exec rake assets:precompile
 echo "running any database migrations..."
 bundle exec rake db:migrate
 
+echo "updating cronjobs..."
+whenever -w
+
 echo "restarting app..."
 touch tmp/restart.txt
 
+echo ""
 echo "WooHoo! Live!"
