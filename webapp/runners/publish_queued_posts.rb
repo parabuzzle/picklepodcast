3ec4caca9874914d9f846bb91bbd8295c5e930fd
Mike Heijmans <parabuzzle@gmail.com>
2013-01-27 07:24:58 -0800

    added social share to queue publisher

--- webapp/runners/publish_queued_posts.rb
+++ webapp/runners/publish_queued_posts.rb
@@ -1,6 +1,26 @@
 # This script publishes a queued post
 # (meant to be ran on cron every sunday @ 3:00p)
 
+require 'json'
+
+def post_to_social(endpoint,params)
+  uri = URI.parse(endpoint)
+ 
+  post_params = { 
+    :title => params[:title],
+    :message => params[:message],
+    :social_token => SOCIAL_TOKEN
+  }
+ 
+  # Convert the parameters into JSON and set the content type as application/json
+  req = Net::HTTP::Post.new(uri.path)
+  req.body = JSON.generate(post_params)
+  req["Content-Type"] = "application/json"
+  
+  http = Net::HTTP.new(uri.host, uri.port)
+  response = http.start {|htt| htt.request(req)}
+end
+
 logger = Rails.logger
 logger.info("[publish_queued_posts] starting...")
 
@@ -18,7 +38,9 @@ unless @posts.length == 0
       logger.error("[publish_queued_posts] error publishing #{post.title}")
     end
   end
-
+  p = Post.find(:all, :conditions=>{:draft=>false}, :order=>:pubdate).last
+  code = post_to_social("http://mikeheymans.com/facebook/social_share", {:title=>p.title,:message=>p.pre})
+  logger.info("[publish_queued_posts] posted to the social share app with return code: #{code}")
   logger.info("[publish_queued_posts] finished publishing posts... exiting")
   logger.flush
 else
